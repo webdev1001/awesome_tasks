287ad65a17e9fff96e9af561a69c4f10087f1bfb
Kasper Johansen <k@spernj.org>
2014-05-26 11:41:51 +0200

    Removed complicated unused method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,44 +31,4 @@ class ApplicationController < ActionController::Base
       "en" => _("English")
     }
   end
-  
-  helper_method :head_title
-  def head_title
-    head_title = ""
-    head_titles = {
-      "customer_search" => Customer.model_name.human(:count => 2),
-      "project_search" => Project.model_name.human(:count => 2),
-      "task_search" => Task.model_name.human(:count => 2),
-      "user_search" => User.model_name.human(:count => 2),
-      "timelog_search" => Timelog.model_name.human(:count => 2),
-      "workstatus" => _("Work-status"),
-      "admin" => _("Administration")
-    }
-    
-    if head_titles.has_key?(params["show"])
-      head_title = head_titles[params["show"]]
-    elsif params["show"] == "task_show" or (params["show"] == "task_edit" and params["task_id"])
-      begin
-        task = _ob.get(:Task, params["task_id"])
-        head_title = task.name
-      rescue Errno::ENOENT
-        flash[:warning] = (_("That task could not be found."))
-        redirect_to :back
-      end
-    elsif params["show"] == "project_show" or (params["show"] == "project_edit" and params["project_id"])
-      begin
-        project = _ob.get(:Project, params["project_id"])
-        head_title = project.name
-      rescue Errno::ENOENT
-        flash[:warning] = (_("That project could not be found."))
-        redirect_to :back
-      end
-    end
-    
-    if head_title.length == 0
-      head_title = "knjTasks"
-    else
-      head_title = "#{head_title} - knjTasks"
-    end
-  end
 end
