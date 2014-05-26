dacf787d6ecab759994cac95476a2b46f2e14510
Kasper Johansen <k@spernj.org>
2014-05-26 17:57:31 +0200

    Added specs for task access. Refactored ability a bit.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,38 +3,13 @@ class Ability
 
   def initialize(current_user)
     if current_user
-      can :show, Task do |task|
-        access = false
-        
-        if task.task_assigned_users.where(:user_id => current_user.id).any?
-          access = true
-        elsif task.project && current_user.user_project_links.where(:project_id => task.project.id).any?
-          access = true
-        elsif task.user == current_user
-          access = true
-        end
-        
-        access
-      end
-      
       can :show, User do |user|
         projects = user.projects
       end
       
-      can [:new, :create, :show], Comment
-      can [:edit, :update, :destroy], Comment do |comment|
-        comment.user_id == current_user.id
-      end
-      
-      can :create, UserTaskListLink
-      can [:edit, :update, :destroy], UserTaskListLink do |user_task_list_link|
-        user_task_list_link.user_id = current_user.id
-      end
-      
-      can :access, :ckeditor
-      can :manage, Ckeditor::Asset
-      can :manage, Ckeditor::AttachmentFile
-      can :manage, Ckeditor::Picture
+      task_access(current_user)
+      ckeditor_access(current_user)
+      comment_access(current_user)
       
       current_user.user_roles.each do |role|
         __send__(role.role)
@@ -58,5 +33,40 @@ private
   
   def customer_administrator
     
+  end
+  
+  def comment_access(current_user)
+    can [:new, :create, :show], Comment
+    can [:edit, :update, :destroy], Comment do |comment|
+      comment.user_id == current_user.id
+    end
+  end
+  
+  def ckeditor_access(current_user)
+    can :access, :ckeditor
+    can :manage, Ckeditor::Asset
+    can :manage, Ckeditor::AttachmentFile
+    can :manage, Ckeditor::Picture
+  end
+  
+  def task_access(current_user)
+    can :create, UserTaskListLink
+    can [:edit, :update, :destroy], UserTaskListLink do |user_task_list_link|
+      user_task_list_link.user_id = current_user.id
+    end
+    
+    can :show, Task do |task|
+      access = false
+      
+      if task.task_assigned_users.where(:user => current_user).any?
+        access = true
+      elsif task.project && current_user.user_project_links.where(:project => task.project).any?
+        access = true
+      elsif task.user == current_user
+        access = true
+      end
+      
+      access
+    end
   end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -25,4 +25,23 @@ describe TasksController do
       # puts ActionMailer::Base.deliveries.last.body
     end
   end
+  
+  context "#show" do
+    let(:user){ create :user }
+    let(:task_access){ create :task, :user => user }
+    let(:task_no_access){ create :task }
+    
+    it "shows tasks that the user have access to" do
+      sign_in user
+      get :show, :id => task_access.id
+      response.should be_success
+    end
+    
+    it "doesnt show tasks that the user doesnt have access to" do
+      sign_in user
+      request.env["HTTP_REFERER"] = root_url
+      get :show, :id => task_no_access.id
+      response.should_not be_success
+    end
+  end
 end
