9080d86877bb84676d6fe4ffd700fa63506d31e6
Kasper Johansen <k@spernj.org>
2014-05-26 17:33:04 +0200

    Refactored and simplified frontpage to use HAML and Ransack.

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -1,68 +1,36 @@
 class FrontpageController < ApplicationController
   def index
-    @tasks = Task.all
-      .includes(:project, :user)
-      .joins("LEFT JOIN task_assigned_users ON task_assigned_users.task_id = tasks.id")
-      .where("tasks.user_id = ? || task_assigned_users.user_id = ?", current_user.id, current_user.id)
-      .group("tasks.id")
-      .where("tasks.state IN (?)", ["new", "open", "waiting"])
+    @ransack_params = params[:q] || {}
     
-    sort_tasks
+    @ransack = Task
+      .related_for_user(current_user)
+      .not_closed
+      .ransack(@ransack_params)
     
-    @headlines = [{
-      "sort" => "name",
-      "sortval" => "name",
-      "title" => _("Task")
-    },{
-      "sort" => "project",
-      "sortval" => {:table => :Project, :col => :name},
-      "title" => _("Project")
-    },{
-      "sort" => "priority",
-      "sortval" => "priority",
-      "title" => _("Priority")
-    },{
-      "sort" => "author",
-      "sortval" => {:table => :User, :col => :name},
-      "title" => _("Author")
-    },{
-      "sort" => "date",
-      "sortval" => "date_added",
-      "title" => _("Date")
-    },{
-      "sort" => "status",
-      "sortval" => "status",
-      "title" => _("Status")
-    }]
-    
-    if can?(:admin, :admin)
-      @headlines += [{
-        "title" => "#{_("Hours")} / #{_("Driving")}"
-      },{
-        "title" => _("Transport length")
-      }]
-    end
+    @tasks = @ransack.result.includes(:project, :user)
+    @tasks = @tasks.order("projects.name, tasks.name") unless params[:q]
   end
   
 private
   
-  def sort_tasks
-    if params["sort"] == "project" or params["sort"].to_s.length <= 0
-      @tasks = @tasks.order("projects.name, tasks.name")
-    elsif params["sort"] == "name"
-      @tasks = @tasks.order("tasks.name")
-    elsif params["sort"] == "date"
-      @tasks = @tasks.order("tasks.created_at")
-    elsif params["sort"] == "author"
-      @tasks = @tasks.joins(:user).order("users.name")
-    elsif params["sort"] == "status"
-      @tasks = @tasks.order("tasks.state")
-    elsif params["sort"] == "priority"
-      @tasks = @tasks.order("tasks.priority")
-    else
-      raise "Dont know how to sort the tasks."
+  helper_method :each_task
+  def each_task
+    @tasks_count = 0
+    @tasks.each do |task|
+      next unless can? :show, task
+      @tasks_count += 1
+      
+      yield(
+        :task => task,
+        :total_length => task.timelogs.sum(:transport_length),
+        :total_hors => task.timelogs.sum(:time).to_f / 3600.0,
+        :total_transport => task.timelogs.sum(:time_transport).to_f / 3600.0
+      )
     end
-    
-    @tasks = @tasks.reverse_order if params["sortmode"] == "desc"
+  end
+  
+  helper_method :no_tasks?
+  def no_tasks?
+    @tasks_count <= 0
   end
 end
--- app/helpers/task_helper.rb
+++ app/helpers/task_helper.rb
@@ -1,5 +1,5 @@
 module TaskHelper
   def link_to_task(task)
-    link_to task.name, task_path(task)
+    link_to task.name, task_path(task), :class => ["task_link", "state_#{task.state}"]
   end
 end
