3ab512544193e1e3a7fb9df64d269105a98d8c6d
kaspernj <k@spernj.org>
2014-05-28 21:20:28 +0200

    Redirect old task urls to new ones.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  before_filter :redirect_old_urls
+  def redirect_old_urls
+    if params[:show] == "tasks_show" && params[:id] && Task.exists?(params[:id])
+      redirect_to task_path(params[:id])
+    end
+  end
+  
   before_filter :check_url
   def check_url
     if !signed_in? && controller_name != "user_authentications"
---
+++ spec/controllers/frontpage_controller_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+
+describe FrontpageController do
+  context "should redirect to new urls when using old ones" do
+    let!(:user){ create :user }
+    let!(:task){ create :task }
+    
+    it "should redirect to new urls" do
+      sign_in user
+      get :index, :show => "tasks_show", :id => task.id
+      response.location.should eq task_url(task)
+    end
+  end
+end
