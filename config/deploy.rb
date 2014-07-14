1fd0ca75898db8e709367f9f1b39ec5df0ae63dc
kaspernj <k@spernj.org>
2014-07-14 21:29:33 +0200

    Link public/system for paperclip uploads.

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,7 @@ set :linked_files, %w{config/database.yml config/smtp.yml config/smtp_defaults.y
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
