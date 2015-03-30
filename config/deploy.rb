4dee956e31900273ced2f7e7c5f796feb9097828
kaspernj <k@spernj.org>
2015-03-30 11:59:11 +0200

    Link PID's, cache and more to ensure Delayed Job restart.

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,8 +26,7 @@ set :repo_url, 'https://github.com/kaspernj/awesome_tasks.git'
 set :linked_files, %w{config/database.yml config/smtp.yml config/smtp_defaults.yml config/smtp_default_url_options.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
