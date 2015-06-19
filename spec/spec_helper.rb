e168bad9c5a56043b482835ceec75fbfd7d26e9e
kaspernj <k@spernj.org>
2015-06-19 09:15:58 +0200

    Require mobile helper since it fails on Shippable

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ CodeClimate::TestReporter.start
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require Rails.root.join('spec', 'support', 'mobile_helper')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -17,14 +18,6 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 RSpec.configure do |config|
   include ActionDispatch::TestProcess
   include MobileHelper
-
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
 
   config.include FactoryGirl::Syntax::Methods
 
